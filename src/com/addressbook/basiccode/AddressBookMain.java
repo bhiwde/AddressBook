740491d766b02c93343efff97a2c594762347bab
bhiwde <nirmlarameshbhiwde@gmail.com>
2023-04-22 20:22:29 +0530

    [ADDED]:[Nirmla] AddressBook Project With Welcome Message

---
+++ src/com/addressbook/basiccode/AddressBookMain.java
@@ -0,0 +1,22 @@
+package com.addressbook.basiccode;
+
+
+
+/***
+ * 
+ * 
+ * @author admin
+ * @since
+ * AddressBookMain Class is used for performing different 
+ * operation on AddressBook
+ * */
+class AddressBookMain {
+
+	public static void main(String[] args)
+	{
+		/* welcome message for user
+		 * 
+		 */
+    System.out.println("Welcome to AddressBook");
+    }
+    }
